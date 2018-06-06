2b4763ab23e32e0c304361f46ea6771b1ecaee9a
Richard L. Taylor Jr <rtaylor@sentryone.com>
2018-06-06 06:15:10 -0400

    Setup part 8 start

---
+++ Part-8-Custom_Renderers-Start/tipcalc/tipcalc.Android/CustomRenderers/BetterEntryRenderer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+//using tipcalc.Droid.CustomRenderers;
+//using tipcalcapp.CustomRenderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+//[assembly: ExportRenderer(typeof(BetterEntry), typeof(BetterEntryRenderer))]
+namespace tipcalc.Droid.CustomRenderers
+{
+    //public class BetterEntryRenderer : EntryRenderer
+    //{
+    //    public BetterEntryRenderer(Context context) : base(context)
+    //    {
+    //    }
+
+    //    protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+    //    {
+    //        base.OnElementChanged(e);
+
+    //        if (Control != null)
+    //        {
+    //            Control.SetBackgroundColor(global::Android.Graphics.Color.Transparent);
+    //        }
+    //    }
+
+    //    protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs args)
+    //    {
+    //        base.OnElementPropertyChanged(sender, args);
+    //        try
+    //        {
+    //            if (args.PropertyName == "IsFocused")
+    //            {
+    //                if (Control.IsFocused)
+    //                {
+    //                    Control.SetBackgroundColor(Android.Graphics.Color.Wheat);
+    //                }
+    //                else
+    //                {
+    //                    Control.SetBackgroundColor(Android.Graphics.Color.Transparent);
+    //                }
+    //            }
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+    //        }
+    //    }
+    //}
+}
---
+++ Part-8-Custom_Renderers-Start/tipcalc_core/Models/TipCalculator.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using tipcalc_core.Interfaces;
+
+namespace tipcalc_core.Models
+{
+    public class TipCalculator : ITipCalculator
+    {
+        private decimal tipPercent;
+
+        public TipCalculator()
+        {
+            NumberOfPersons = 1;
+        }
+
+        public decimal Total { get; set; }
+
+        public decimal Tip { get; set; }
+
+        public decimal SavedTip { get; set; }
+
+        public decimal TipPercent
+        {
+            get { return tipPercent; }
+            set { tipPercent = Math.Round(value); }
+        }
+
+        public decimal SavedGrandTotal { get; private set; }
+
+        public decimal GrandTotal { get; private set; }
+
+        public int NumberOfPersons { get; set; }
+
+        public decimal TotalPerPerson { get; private set; }
+
+        public void CalcTip()
+        {
+            if (tipPercent > 0)
+            {
+                Tip = Math.Round(Total * (tipPercent / 100), 2);                
+            }
+            else
+            {
+                Tip = 0;
+            }
+            UpdateGrandTotal();
+        }
+
+        public void CalcTipPercentage()
+        {
+            if (Total > 0)
+            {
+                tipPercent = Math.Round((Tip / Total) * 100, 1);
+            }
+            else
+            {
+                tipPercent = 0;
+            }
+
+            UpdateGrandTotal();
+        }
+
+        public void RoundTotal()
+        {
+            SavedGrandTotal = GrandTotal;
+            SavedTip = Tip;
+            GrandTotal = Math.Round(GrandTotal);
+            Tip = (GrandTotal > Total) ? GrandTotal - Total : 0 ;
+            SplitGrandTotal();
+        }
+
+        public void UnRoundTotal()
+        {
+            GrandTotal = SavedGrandTotal;
+            SavedGrandTotal = 0;
+            Tip = SavedTip;
+            SavedTip = 0;
+            SplitGrandTotal();
+        }
+
+        public void SplitGrandTotal()
+        {
+            TotalPerPerson = Math.Round((GrandTotal / NumberOfPersons), 2);
+        }
+
+        public void Reset()
+        {
+            Total = 0;
+            Tip = 0;
+            TipPercent = 0;
+            GrandTotal = 0;
+            NumberOfPersons = 1;
+            TotalPerPerson = 0;
+            SavedGrandTotal = 0;
+            SavedTip = 0;
+        }
+        
+        private void UpdateGrandTotal()
+        {
+            GrandTotal = Total + Tip;
+            SplitGrandTotal();
+        }
+    }
+}
