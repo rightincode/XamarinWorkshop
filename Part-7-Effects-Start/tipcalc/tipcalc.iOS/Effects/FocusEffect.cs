6a87a65ea82419a9a72aa535f639f6165e84c2d1
Richard L. Taylor Jr <rtaylor@sentryone.com>
2018-06-06 05:02:16 -0400

    Removed effects for start of section

---
+++ Part-7-Effects-Start/tipcalc/tipcalc.iOS/Effects/FocusEffect.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using tipcalc.iOS.Effects;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+//[assembly: ResolutionGroupName("MyCompany")]
+//[assembly: ExportEffect(typeof(FocusEffect), "FocusEffect")]
+namespace tipcalc.iOS.Effects
+{
+    //public class FocusEffect : PlatformEffect
+    //{
+    //    UIColor backgroundColor;
+
+    //    protected override void OnAttached()
+    //    {
+    //        try
+    //        {
+    //            Control.BackgroundColor = backgroundColor = UIColor.FromRGB(204, 153, 255);
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+    //        }
+    //    }
+
+    //    protected override void OnDetached()
+    //    {
+    //    }
+
+    //    protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+    //    {
+    //        base.OnElementPropertyChanged(args);
+
+    //        try
+    //        {
+    //            if (args.PropertyName == "IsFocused")
+    //            {
+    //                if (Control.BackgroundColor == backgroundColor)
+    //                {
+    //                    Control.BackgroundColor = UIColor.White;
+    //                }
+    //                else
+    //                {
+    //                    Control.BackgroundColor = backgroundColor;
+    //                }
+    //            }
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+    //        }
+    //    }
+    //}
+}
---
+++ Part-7-Effects-Start/tipcalcapp/Interfaces/IAuthenticate.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Identity.Client;
+using System.Threading.Tasks;
+
+namespace tipcalc.Interfaces
+{
+    public interface IAuthenticate
+    {
+        PublicClientApplication AuthClient { get; set; }
+
+        Task<bool> LoginAsync(bool useSilent = false);
+
+        Task<bool> LogoutAsync();
+
+        bool Logout();
+    }
+}
