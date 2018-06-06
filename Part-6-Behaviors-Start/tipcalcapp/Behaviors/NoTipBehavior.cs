5869fe5a0b454950f1578f3460c3eee71a663656
Richard L. Taylor Jr <rtaylor@sentryone.com>
2018-06-06 15:01:19 -0400

    csproj fixes to remove references to 26

--- Part-6-Behaviors-Start/tipcalcapp/Behaviors/NoTipBehavior.cs
+++ Part-6-Behaviors-Start/tipcalcapp/Behaviors/NoTipBehavior.cs
@@ -2,32 +2,32 @@
 
 namespace tipcalcapp.Behaviors
 {
-    //public class NoTipBehavior : Behavior<Entry>
-    //{
-    //    protected override void OnAttachedTo(Entry entry)
-    //    {
-    //        entry.TextChanged += OnEntryTextChanged;
-    //        base.OnAttachedTo(entry);
-    //    }
+    public class NoTipBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
 
-    //    protected override void OnDetachingFrom(Entry entry)
-    //    {
-    //        entry.TextChanged -= OnEntryTextChanged;
-    //        base.OnDetachingFrom(entry);
-    //    }
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
 
-    //    void OnEntryTextChanged(object sender, TextChangedEventArgs args)
-    //    {
-    //        bool isValid = double.TryParse(args.NewTextValue, out double result);
+        void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            bool isValid = double.TryParse(args.NewTextValue, out double result);
 
-    //        if ((isValid) && (result <= 0))
-    //        {
-    //            ((Entry)sender).TextColor = Color.Red;
-    //        }
-    //        else
-    //        {
-    //            ((Entry)sender).TextColor = Color.Default;
-    //        }
-    //    }
-    //}
+            if ((isValid) && (result <= 0))
+            {
+                ((Entry)sender).TextColor = Color.Red;
+            }
+            else
+            {
+                ((Entry)sender).TextColor = Color.Default;
+            }
+        }
+    }
 }
