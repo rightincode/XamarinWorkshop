f8e74d0d86fede20a6ce13200ab2f8980a94f431
Richard L. Taylor Jr <rtaylor@sentryone.com>
2018-06-06 04:55:29 -0400

    Added focus effect for UWP

---
+++ Part-7-Effects-Final/tipcalc/tipcalc.UWP/Effects/FocusEffect.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tipcalc.UWP.Effects;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ResolutionGroupName("MyCompany")]
+[assembly: ExportEffect(typeof(FocusEffect), "FocusEffect")]
+namespace tipcalc.UWP.Effects
+{
+    public class FocusEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            try
+            {
+                (Control as Windows.UI.Xaml.Controls.Control).Background = new SolidColorBrush(Colors.Cyan);
+                (Control as FormsTextBox).BackgroundFocusBrush = new SolidColorBrush(Colors.White);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
+        }
+
+        protected override void OnDetached()
+        {
+        }
+    }
+}
