7d3e8cb23e440c3a3d9a4b03da506c5f41a2938f
Richard L. Taylor Jr <rtaylor@sentryone.com>
2018-06-05 23:46:34 -0400

    Updates for SyntaxCon 2018

---
+++ Part-4-Clean_Architecture-Final/TipCalc_core_tests/TipCalculatorTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TipCalc_core.Models;
+
+namespace TipCalc_core_tests
+{
+    [TestClass]
+    public class TipCalculatorTests
+    {
+        [TestMethod]
+        public void CalcTip_18PercentTip100Total_GrandTotal118()
+        {
+            var tipCalculator = new TipCalculator
+            {
+                Total = 100,
+                TipPercent = 18
+            };
+
+            tipCalculator.CalcTip();
+
+            Assert.AreEqual(decimal.Parse("118"), tipCalculator.GrandTotal);
+        }
+    }
+}
---
+++ Part-4-Clean_Architecture-Final/TipCalc_viewmodels_tests/MainPageViewModelTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TipCalc.ViewModels;
+using TipCalc_core.Models;
+
+namespace TipCalc_viewmodels_tests
+{
+    [TestClass]
+    public class MainPageViewModelTests
+    {
+        [TestMethod]
+        public void CalcTip_18PercentTip100Total_GrandTotal118()
+        {
+            var mainPageViewModel = new MainPageViewModel(new TipCalculator());
+
+            mainPageViewModel.TotalTxt = "100";
+            mainPageViewModel.TipPercent = 18;
+
+            Assert.AreEqual("118.00", mainPageViewModel.GrandTotalTxt);       
+
+        }
+    }
+}
